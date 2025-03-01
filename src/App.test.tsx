d4144af741f79d9280f8439ee71691f105a885db
mrfachri19 <fachrimaulanapakpahan@gmail.com>
2025-03-01 17:52:28 +0700

    fix: change language test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 
 describe("GitHub Repositories Explorer", () => {
-  test("menampilkan input dan tombol search", () => {
+  test("Show input and Search button", () => {
     render(<App />);
     expect(screen.getByPlaceholderText("Enter username")).toBeInTheDocument();
     expect(screen.getByText("Search")).toBeInTheDocument();
